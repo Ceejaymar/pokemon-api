913fd31a0f2e7cadad8518ad34b24f092b71ff66
Carlos Martinez <martinezcj2@gmail.com>
2019-02-15 22:50:27 -0500

    Create get, put, delete routes

--- routes/trainer.js
+++ routes/trainer.js
@@ -1,16 +1,76 @@
 const express = require('express');
 const trainerRouter = express.Router();
+const TrainerService = require('../services/trainer');
 
-trainerRouter.get('/', (req, res, next) => {
-  const { name } = req.params;
+trainerRouter.post('/' , (req, res, next) => {
+  const { name, hometown } = req.body;
 
-  trainerService.read(name)
+  TrainerService.create(name, hometown)
     .then(data => {
-      res.json({success: 'We hit this route'})
+      res.json({ success: `Created trainer with generated ID: ${data.id}` })
     })
     .catch(err => {
       next(err);
     })
 });
+
+trainerRouter.get('/:name', (req, res, next) => {
+  const { name } = req.params;
+
+  TrainerService.read(name)
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => {
+      next(err);
+    })
+});
+
+trainerRouter.put('/:name', (req, res, next) => {
+  const { name } = req.params;
+  const { hometown } = req.body;
+
+  TrainerService.update(name, hometown)
+    .then(data => {
+      res.json({ success: `Updated trainer named ${name}` })
+    })
+})
+
+trainerRouter.delete('/:name', (req, res, next) => {
+  const { name } = req.params;
+  
+  TrainerService.delete(name)
+    .then(data => {
+      res.json({ success: `Deleted trainer named ${name}` })
+    })
+    .catch(err => {
+      next(err);
+    })
+})
+
+trainerRouter.get('/:name/pokemons', (req, res, next) => {
+  const { name }  = req.params;
+  const { levelmin } = req.query;
+  
+  if(levelmin) {
+    TrainerService.getLevelPokemons(name, levelmin)
+      .then(data => {
+        res.json({ data });
+      })
+      .catch(err => {
+        next(err);
+      })
+  }
+  else {
+    TrainerService.getAllPokemons(name)
+      .then(data => {
+        res.json({ data });
+      })
+      .catch(err => {
+        next(err);
+      })
+  }
+});
+
 
 module.exports = trainerRouter;
